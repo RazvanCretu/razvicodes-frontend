2fbf5197710d02d4e52606aa17cface33552003a
Razvy Cretu <razvan.cretu97@gmail.com>
2023-01-16 20:01:55 +0200

    migrate articles List to MUI

--- src/components/Articles/List.js
+++ src/components/Articles/List.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
+import {
+  List as MUIList,
+  ListItem,
+  Card,
+  CardActions,
+  Divider,
+  Typography,
+  Button,
+  Box,
+} from "@mui/material";
 import "moment-timezone";
-
-import styles from "../../styles/Articles/List.module.css";
 
 Moment.globalLocal = true;
 
@@ -11,25 +19,36 @@ const List = ({ articles }) => {
   const animate = (i) => `slideInRight 1s ease-in ${0.25 * i}s forwards`;
 
   return (
-    <ul className={styles.list}>
-      {articles.map((article, i, arr) => (
-        <li
-          className={styles.item}
-          style={{ animation: animate(i) }}
-          key={article.id}
-        >
-          <div className={styles.itemHero}>
-            <h2 className={styles.itemTitle}>{article.attributes.title}</h2>
-            <Moment className={styles.itemTime} format="YY MMM dddd Do">
-              {article.attributes.publishedAt}
-            </Moment>
-          </div>
-          <Link className={styles.readMore} to={article.attributes.slug}>
-            Read
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <MUIList>
+      {articles.map(
+        ({ id, attributes: { title, publishedAt, slug } }, i, arr) => (
+          <ListItem sx={{ animation: animate(i) }} key={id}>
+            <Card sx={{ display: "flex", padding: "1rem" }}>
+              <Box>
+                <Typography variant="h4">{title}</Typography>
+                <Typography
+                  component={Moment}
+                  sx={{ fontStyle: "italic" }}
+                  format="YY MMM dddd Do"
+                >
+                  {publishedAt}
+                </Typography>
+              </Box>
+              <Divider
+                orientation="vertical"
+                flexItem
+                sx={{ margin: "0 1rem 0 5rem" }}
+              />
+              <CardActions>
+                <Button variant="contained" component={Link} to={slug}>
+                  Read
+                </Button>
+              </CardActions>
+            </Card>
+          </ListItem>
+        )
+      )}
+    </MUIList>
   );
 };
 
