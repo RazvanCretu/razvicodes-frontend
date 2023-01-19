1fd54f80476993374dc431aec29da665e71c1611
Razvy Cretu <razvan.cretu97@gmail.com>
2023-01-19 20:21:54 +0200

    delete custom Container

--- src/components/About/Container.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "../../styles/About/Container.module.css";
-
-const Container = ({ children }) => {
-  return <div className={styles.About}>{children}</div>;
-};
-
-export default Container;
