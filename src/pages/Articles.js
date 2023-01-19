a2303ffeba6f01ec4da3bdb223dcbd34ff53644d
Razvy Cretu <razvan.cretu97@gmail.com>
2023-01-19 14:28:43 +0200

    make pagination client-side only

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useState } from "react";
 import { Container, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import List from "../components/Articles/List";
@@ -8,25 +8,14 @@ import Pagination from "../components/Pagination";
 import { useQuery, gql } from "@apollo/client";
 
 const QUERY_ARTICLES = gql`
-  query Articles($pageSize: Int, $page: Int) {
-    articles(
-      pagination: { page: $page, pageSize: $pageSize }
-      sort: "publishedAt:desc"
-    ) {
+  query Articles {
+    articles(sort: "publishedAt:desc", pagination: { limit: -1 }) {
       data {
         id
         attributes {
           title
           slug
           publishedAt
-        }
-      }
-      meta {
-        pagination {
-          total
-          page
-          pageSize
-          pageCount
         }
       }
     }
@@ -42,10 +31,8 @@ const ArticlesContainer = styled(Container)(({ theme }) => ({
   flexDirection: "column",
   height: "100vh",
   margin: "0 auto",
-  // paddingTop: "7vh",
   position: "relative",
   overflow: "hidden",
-  // paddingBottom: "3rem",
   padding: "7vh 5px 3rem",
   [theme.breakpoints.up("md")]: {
     maxWidth: "1000px",
@@ -54,30 +41,41 @@ const ArticlesContainer = styled(Container)(({ theme }) => ({
 
 const Articles = () => {
   const [currentPage, setCurrentPage] = useState(getDefaultPage());
-  const { error, loading, data } = useQuery(QUERY_ARTICLES, {
-    variables: { page: currentPage, pageSize: pageSize },
-  });
+  const { error, loading, data } = useQuery(QUERY_ARTICLES);
 
-  useEffect(() => {
-    localStorage.setItem("articlesPage", currentPage);
-  }, [currentPage]);
+  const lastPost = currentPage * pageSize;
+  const firstPost = lastPost - pageSize;
+  const posts = data?.articles.data.slice(firstPost, lastPost);
+
+  const handlePageChange = (page) => {
+    localStorage.setItem("articlesPage", page);
+    setCurrentPage(page);
+  };
+
+  if (error) {
+    return <Typography>{error.message}</Typography>;
+  }
+
+  if (loading) {
+    return (
+      <ArticlesContainer>
+        <Loader />
+      </ArticlesContainer>
+    );
+  }
 
   return (
     <ArticlesContainer>
-      {error && <Typography>{error.message}</Typography>}
-      {loading && <Loader />}
-      {data && (
-        <Fragment>
-          <List articles={data.articles.data} />
-          <Pagination
-            className="pagination-bar"
-            currentPage={currentPage}
-            totalCount={data?.articles.meta.pagination.total}
-            pageSize={pageSize}
-            onPageChange={(page) => setCurrentPage(page)}
-          />
-        </Fragment>
-      )}
+      <Fragment>
+        <List articles={posts} />
+        <Pagination
+          className="pagination-bar"
+          currentPage={currentPage}
+          totalCount={data.articles.data.length}
+          pageSize={pageSize}
+          onPageChange={handlePageChange}
+        />
+      </Fragment>
     </ArticlesContainer>
   );
 };
