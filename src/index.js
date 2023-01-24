b8db6e376674a536459d38a173442694d304175a
Razvy Cretu <razvan.cretu97@gmail.com>
2023-01-25 00:13:29 +0200

    add Roboto font

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import Theme from "./theme";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 const client = new ApolloClient({
   uri: `${process.env.REACT_APP_BACKEND_GRAPHQL}`,
   cache: new InMemoryCache(),
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <Router>
     <ApolloProvider client={client}>
