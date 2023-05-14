126d5f04b4624f4625d38da5c9d4192e71e831bf
RazvanCretu <razvan.cretu97@gmail.com>
2023-05-14 18:22:39 +0300

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  module: {
+    loaders: [
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
